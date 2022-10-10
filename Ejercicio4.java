985a13d9c541f87e6b8502f050daa658b51487dd
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 10:00:55 +0200

    Ejercicio4

---
+++ Ejercicio4.java
@@ -0,0 +1,25 @@
+/**
+ * 1 Escribe un programa que sume, reste, multiplique y divida dos números introducidos por teclado.
+ * 
+ * @author Javier Miguel Martín Gallardo
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dime un número: ");
+        float n = sc.nextInt();
+        System.out.print("Dime otro número: ");
+        float c = sc.nextInt();
+        
+        System.out.println("Dado esos números: ");
+        System.out.println("La suma es: "+ (n+c));
+        System.out.println("La resta es: " + (n-c));
+        System.out.println("La multiplicación es:"+ (n*c));
+        System.out.println("La división es: "+(n/c));
+
+        sc.close();
+    }
+}

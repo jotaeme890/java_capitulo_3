d43e926d225df666e9e32e28f405029973a239a7
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 18:47:54 +0200

    Ejercicio12

---
+++ Ejercicio12.java
@@ -0,0 +1,22 @@
+/**
+ * 1 Realiza un programa que calcule la nota que hace falta sacar en el segundo examen de la asignatura Programación 
+ * para obtener la media deseada. Hay que tener en cuenta que la nota del primer examen cuenta el 40% y 
+ * la delsegundo examen un 60%.
+ * 
+ * @author Jotaeme
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio12 {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Qué has sacado en el primer examen?");
+        double n = sc.nextDouble();
+        System.out.println("¿Qué quieres sacar en la evaluación?");
+        double c = sc.nextDouble();
+
+        System.out.print("La nota que tienes que sacar es: " + ((c-n*0.40)/0.60));
+        sc.close();
+    }
+}

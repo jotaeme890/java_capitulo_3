fbcf18d8909f9652fc3503d3683748ddadde73e0
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 18:32:45 +0200

    Ejercicio10

---
+++ Ejercicio10.java
@@ -0,0 +1,19 @@
+/**
+ * 1 Realiza un conversor de Mb a Kb.
+ * 
+ * @author Jotaeme
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dime la Mb que quieres pasasr a kb: ");
+        float n = sc.nextInt();
+        float c = n*1000;
+
+        System.out.print("El resultado de dicha conversión es: " + c);
+        sc.close();
+    }
+}

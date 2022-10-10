b0f74b1a74ff1557e69f94e386b4480b5a1232b4
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 10:25:44 +0200

    Ejercicio9

---
+++ Ejercicio9.java
@@ -0,0 +1,23 @@
+/**
+ * 1 Escribe un programa que calcule el volumen de un cono según la fórmula V = 1|3πr2h
+ * 
+ * @author Javier Miguel Martín Gallardo
+ */
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Ejercicio9 {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime la altura del cono: ");
+        double h = sc.nextInt();
+        System.out.println("Dime el radio del cono: ");
+        double r = sc.nextInt();
+        double partido= (1.0/3.0);
+
+        System.out.print("El volumen del cono es: " + (partido*Math.PI*(r*r)*h));
+
+        sc.close();
+    }
+}

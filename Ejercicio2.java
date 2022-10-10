1e7ee0f5307e9d04b61e574f01816dfa47c835e1
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 09:47:03 +0200

    Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,20 @@
+/**
+ * 1 Convertor de euros a pesetas, cogiendo los euros por el teclado
+ * 
+ * @author Javier Miguel Martín Gallardo
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dime la cantidad de euros: ");
+        double n = sc.nextInt();
+        double pesetas = 166.38;
+        
+        System.out.print("La cantidad de "+ n + " euros en pesetas, son: " + n*pesetas);
+        
+        sc.close();
+    }
+}

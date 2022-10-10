27dd412bcdbba3b4988995ed30a1aeaf9ed44080
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 18:21:48 +0200

    Ejercicio8

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,7 +1,7 @@
 /**
  * 1 Escribe un programa que calcule el salario semanal de un empleado en base a las horas trabajadas, a razón de 12 euros la hora.
  * 
- * @author Javier Miguel Martín Gallardo
+ * @author Jotaeme
  */
 
 import java.util.Scanner;

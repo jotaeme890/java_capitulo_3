9635fa9329f4da8f75529314db6055aa4155f46c
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 18:29:12 +0200

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -10,7 +10,7 @@ public class Ejercicio3 {
     public static void main (String args []){
         Scanner sc = new Scanner(System.in);
         System.out.print("Dime la cantidad de pesetas: ");
-        double n = sc.nextInt();
+        double n = sc.nextDouble();
         double euros = 0.0060;
         
         System.out.print("La cantidad de "+ n + " pesetas en euros, son: " + n*euros);

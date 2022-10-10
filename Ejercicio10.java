b4b99ba493ad58b1fcf353188d2128bad0a67c49
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 18:35:42 +0200

    Ejercicio10

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Ejercicio10 {
     public static void main (String args []){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Dime la Mb que quieres pasasr a kb: ");
+        System.out.print("Dime la Mb que quieres pasasr a Kb: ");
         float n = sc.nextInt();
         float c = n*1000;
 
